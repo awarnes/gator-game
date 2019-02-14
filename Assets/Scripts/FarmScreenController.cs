fcc273ccc727af4a89d1b5b974c67f5b44ac74ab
Alexander Warnes <warnes.alexander@gmail.com>
2019-02-13 17:09:26 -0800

    Commit minor changes.

--- Assets/Scripts/FarmScreenController.cs
+++ Assets/Scripts/FarmScreenController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class FarmScreenController : MonoBehaviour
-{
+namespace GatorGame {
+    public class FarmScreenController : MonoBehaviour {
 
-    
+    }
 }
